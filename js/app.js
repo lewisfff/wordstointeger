dd5a9b66463a3eb0b31e64bdc00fa5a553e10c2c
lewis <16711731@protonmail.com>
2017-06-15 09:36:16 +0100

    Application stucturing Data objects added App class methods

--- js/app.js
+++ js/app.js
@@ -1,7 +1,56 @@
+
+const data = require('./data.js');
+
 class WordsToNumberApp {
     constructor() {
-        debugger;
+        this.numbers = data.numbers;
+        this.multiples = data.multiples;
+    }
+
+    convert(wordString) {
+        let tokens = this.wordSplit(wordString);
+
+        if (this.validateInput(tokens))
+            return this.calcNumber(tokens);
+    }
+
+    validateInput(tokens) {
+        let valid = true;
+
+        tokens.forEach((item) => {
+            if (this.numbers[item]) {
+            } else if (this.multiples[item]) {
+            } else if (item === 'and') {
+            } else {
+                this.showInputError(item);
+                valid = false;
+            }
+        });
+
+        return valid;
+    }
+
+    showInputError(item) {
+        alert(item + ' not accounted for');
+    }
+
+    wordSplit(wordString) {
+        return wordString.toLowerCase().split(' ');
+    }
+
+    calcNumber(tokens) {
+        let result;
+
+        // loop through and perform arithmetic
+        result = tokens;
+
+        this.showResult(result);
+    }
+
+    showResult(result) {
+        // display the result on the web page
+        console.log(result);
     }
 }
 
-window.addEventListener('load', () => new WordsToNumberApp());
+window.addEventListener('load', () => window.wtna = new WordsToNumberApp());
--- public/app.js
+++ public/app.js
@@ -68,18 +68,78 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var WordsToNumberApp = function WordsToNumberApp() {
-    _classCallCheck(this, WordsToNumberApp);
+var data = __webpack_require__(7);
 
-    debugger;
-};
+var WordsToNumberApp = function () {
+    function WordsToNumberApp() {
+        _classCallCheck(this, WordsToNumberApp);
+
+        this.numbers = data.numbers;
+        this.multiples = data.multiples;
+    }
+
+    _createClass(WordsToNumberApp, [{
+        key: 'convert',
+        value: function convert(wordString) {
+            var tokens = this.wordSplit(wordString);
+
+            if (this.validateInput(tokens)) return this.calcNumber(tokens);
+        }
+    }, {
+        key: 'validateInput',
+        value: function validateInput(tokens) {
+            var _this = this;
+
+            var valid = true;
+
+            tokens.forEach(function (item) {
+                if (_this.numbers[item]) {} else if (_this.multiples[item]) {} else if (item === 'and') {} else {
+                    _this.showInputError(item);
+                    valid = false;
+                }
+            });
+
+            return valid;
+        }
+    }, {
+        key: 'showInputError',
+        value: function showInputError(item) {
+            alert(item + ' not accounted for');
+        }
+    }, {
+        key: 'wordSplit',
+        value: function wordSplit(wordString) {
+            return wordString.toLowerCase().split(' ');
+        }
+    }, {
+        key: 'calcNumber',
+        value: function calcNumber(tokens) {
+            var result = void 0;
+
+            // loop through and perform arithmetic
+            result = tokens;
+
+            this.showResult(result);
+        }
+    }, {
+        key: 'showResult',
+        value: function showResult(result) {
+            // display the result on the web page
+            console.log(result);
+        }
+    }]);
+
+    return WordsToNumberApp;
+}();
 
 window.addEventListener('load', function () {
-    return new WordsToNumberApp();
+    return window.wtna = new WordsToNumberApp();
 });
 
 /***/ }),
@@ -94,6 +154,59 @@ window.addEventListener('load', function () {
 
 __webpack_require__(0);
 module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 3 */,
+/* 4 */,
+/* 5 */,
+/* 6 */,
+/* 7 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "numbers", function() { return numbers; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "multiples", function() { return multiples; });
+
+var numbers = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    'ten': 10,
+    'eleven': 11,
+    'twelve': 12,
+    'thirteen': 13,
+    'fourteen': 14,
+    'fifteen': 15,
+    'sixteen': 16,
+    'seventeen': 17,
+    'eighteen': 18,
+    'nineteen': 19,
+    'twenty': 20,
+    'thirty': 30,
+    'forty': 40,
+    'fifty': 50,
+    'sixty': 60,
+    'seventy': 70,
+    'eighty': 80,
+    'ninety': 90
+};
+
+var multiples = {
+    'thousand': 1000,
+    'million': 1000000,
+    'billion': 1000000000,
+    'trillion': 1000000000000
+};
+
 
 
 /***/ })
